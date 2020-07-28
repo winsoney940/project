166611b4ce6bfa7707459f70d2fca92b5cf3f057
Georgiy <kositskiy2003@mail.ru>
2020-07-28 17:54:22 +0300

    second task

--- js/script.js
+++ js/script.js
@@ -65,6 +65,14 @@
 
 const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
 
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
 
         
     
